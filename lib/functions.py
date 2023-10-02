851f69184a7914c30484eee6b8a2c04d9e8cbf5d
Wakio37 <your_email@example.com>
2023-10-02 19:59:09 +0000

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    return "Hello, programmer!\n"
+
 
 def greet(name):
-    pass
+    return f"Hello, {name}!"
 
 def greet_with_default(name="programmer"):
-    pass
+    return f"Hello, {name}!"
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
+
+print(greet_programmer())               
+print(greet("Naureen"))                 
+print(greet_with_default())              
+print(greet_with_default("Sunny"))       
+print(add(1, 2))                        
+print(halve(4))                       
+print(halve("two"))       
