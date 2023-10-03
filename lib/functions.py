7f3f84b58faf0349da8dc62677a85d7e29caadb1
Wakio37 <your_email@example.com>
2023-10-03 17:34:23 +0000

    completed assinment

--- lib/functions.py
+++ lib/functions.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    return "Hello, programmer!\n"
+    print("Hello, programmer!")
 
 
 def greet(name):
-    return f"Hello, {name}!"
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    return f"Hello, {name}!"
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     return num1 + num2
@@ -16,10 +16,3 @@ def add(num1, num2):
 def halve(number):
     return number/2
 
-print(greet_programmer())               
-print(greet("Naureen"))                 
-print(greet_with_default())              
-print(greet_with_default("Sunny"))       
-print(add(1, 2))                        
-print(halve(4))                       
-print(halve("two"))       
